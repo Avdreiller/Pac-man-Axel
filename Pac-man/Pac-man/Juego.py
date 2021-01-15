51aa1731722e4e5da46eea77b775c6375489e99a
andres7xd <58347580+andres7xd@users.noreply.github.com>
2021-01-14 18:26:03 -0600

    movimiento sensual del pacman(jutsu multiclones de sombras)

--- Pac-man/Pac-man/Juego.py
+++ Pac-man/Pac-man/Juego.py
@@ -64,6 +64,10 @@ class Juego(object):
         
 
     def pantalla_juego(self):
+        posicion_x_pacman = 0
+        posicion_y_pacman = 0
+        bandera = True
+
         self.llenar_lista_nodos(self)
         m = Mapa.Mapa
         m.generar_Mapa(m)
@@ -76,17 +80,22 @@ class Juego(object):
         tamCuadro = 21
  
 
+        
         size = (820, 720)
         screen = pygame.display.set_mode(size)
+        pygame.key.set_repeat(500,30)
         pygame.display.set_caption("Grid on PYGAME")
         clock = pygame.time.Clock()
         gameOver = True
         logo = pygame.image.load("recursos/muros/MuroSolo.png")
         logo = pygame.transform.scale(logo, (tamCuadro,tamCuadro))
         while gameOver:
+           
             for event in pygame.event.get():
+                self.moverPacman(self,event,posicion_x_pacman,posicion_y_pacman,bandera)
                 if event.type == pygame.QUIT:
                     gameOver = False
+            bandera = True
             screen.fill(BLACK)
             i1 = 0
             for i in range(1, 500, tamCuadro + 1):
@@ -100,6 +109,8 @@ class Juego(object):
                                 if img != None:
                                     screen.blit(img.imagen, (i+155, j+105))
                             if self.mapaJ[j1][i1]=='@':
+                                posicion_x_pacman = j1
+                                posicion_y_pacman = i1
                                 imagen = Imagen.Imagen("otros/PacMan", i+160, j+110, tamCuadro+8, tamCuadro+8)
                                 screen.blit(imagen.imagen, (i+156, j+106))
                             if self.mapaJ[j1][i1]=='_' or self.mapaJ[j1][i1]=='-':
@@ -112,3 +123,35 @@ class Juego(object):
             clock.tick(1)
         pygame.quit()
 
+
+    def moverPacman(self,evento,posicion_x_pacman,posicion_y_pacman,bandera):
+        if evento.type == pygame.KEYUP and bandera == True:
+            bandera = False
+            if evento.key == pygame.K_RIGHT:
+                if(self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]!='#'):
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]='@'
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+            
+            elif evento.key == pygame.K_LEFT:
+                if(self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]!='#'):
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]='@'
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+            elif evento.key == pygame.K_DOWN:
+                if(self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]!='#'):
+                    self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]='@'
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+            elif evento.key == pygame.K_UP:
+                if(self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]!='#'):
+                    self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]='@'
+                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+    #    if evento.type == pygame.keyup:
+    #        if evento.key == pygame.k_right:
+    #            velocidad_auto_x = 0
+    #        if evento.key == pygame.k_left:
+    #            velocidad_auto_x = 0
+    #        if evento.key == pygame.k_down:
+    #            velocidad_auto_y = 0
+    #        if evento.key == pygame.k_up:
+    #            velocidad_auto_y = 0
+    #auto[0] += velocidad_auto_x
+    #auto[1] += velocidad_auto_y
