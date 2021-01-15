a54ccbda522f6c5d974cba37a8578198e880b493
Avdreiller <64115743+Avdreiller@users.noreply.github.com>
2021-01-14 19:07:53 -0600

    Arreglo de movimiento

--- Pac-man/Pac-man/Juego.py
+++ Pac-man/Pac-man/Juego.py
@@ -14,7 +14,7 @@ class Juego(object):
         contador = 1
         for i in range(len(m)):
             for j in range(len(m)):
-                n = Nodo.Nodo(contador, m[i][j])
+                n = Nodo.Nodo(contador, m[i][j], None)
                 self.nodos.append(n)
                 contador += 1
     def validar(self,mposx,mposy,posx,posy):
@@ -79,71 +79,99 @@ class Juego(object):
         BLACK = (0, 0, 0)
         tamCuadro = 21
  
-
+        mapa_imagenes = []
+        for x in range(23):
+            mapa_imagenes.append([])
+            for j in range(23):
+                mapa_imagenes[x].append(Nodo.Nodo(0,'',None))
         
         size = (820, 720)
         screen = pygame.display.set_mode(size)
-        pygame.key.set_repeat(500,30)
+        #pygame.key.set_repeat(500,30)
         pygame.display.set_caption("Grid on PYGAME")
         clock = pygame.time.Clock()
         gameOver = True
         logo = pygame.image.load("recursos/muros/MuroSolo.png")
         logo = pygame.transform.scale(logo, (tamCuadro,tamCuadro))
+        primera_vandera = True
         while gameOver:
            
             for event in pygame.event.get():
-                self.moverPacman(self,event,posicion_x_pacman,posicion_y_pacman,bandera)
+                if event.type == pygame.KEYDOWN and bandera == True:
+                    bandera = False
+                    self.moverPacman(self,event,posicion_x_pacman,posicion_y_pacman,bandera)
                 if event.type == pygame.QUIT:
                     gameOver = False
             bandera = True
             screen.fill(BLACK)
             i1 = 0
+            id = 1
             for i in range(1, 500, tamCuadro + 1):
                 j1 = 0
                 if i1 < 23:
                     for j in range(1, 500, tamCuadro+1):
                         #pygame.draw.rect(screen, AZUL, [i, j, tamCuadro, tamCuadro], 0)
                         if j1 < 23:
-                            if self.mapaJ[j1][i1]=='#':
-                                img = self.asignar_Muro(self,j1,i1,i,j,tamCuadro+10)
-                                if img != None:
-                                    screen.blit(img.imagen, (i+155, j+105))
-                            if self.mapaJ[j1][i1]=='@':
-                                posicion_x_pacman = j1
-                                posicion_y_pacman = i1
-                                imagen = Imagen.Imagen("otros/PacMan", i+160, j+110, tamCuadro+8, tamCuadro+8)
-                                screen.blit(imagen.imagen, (i+156, j+106))
-                            if self.mapaJ[j1][i1]=='_' or self.mapaJ[j1][i1]=='-':
-                                imagen = Imagen.Imagen("otros/bolas", i+160, j+110, tamCuadro, tamCuadro)
-                                screen.blit(imagen.imagen, (i+160, j+110))
+                            if primera_vandera == True:
+                                if self.mapaJ[j1][i1]=='#':
+                                    img = self.asignar_Muro(self,j1,i1,i,j,tamCuadro+10)
+                                    n = Nodo.Nodo(id,self.mapaJ[j1][i1],img)
+                                    mapa_imagenes[j1][i1] = n
+                                    if img != None:
+                                        screen.blit(img.imagen, (i+155, j+105))
+                                if self.mapaJ[j1][i1]=='@':
+                                    posicion_x_pacman = j1
+                                    posicion_y_pacman = i1
+                                    imagen = Imagen.Imagen("otros/PacMan", i+160, j+110, tamCuadro+8, tamCuadro+8)
+                                    screen.blit(imagen.imagen, (i+156, j+106))
+                                    n = Nodo.Nodo(id,self.mapaJ[j1][i1],imagen)
+                                    mapa_imagenes[j1][i1] = n
+                                if self.mapaJ[j1][i1]=='_' or self.mapaJ[j1][i1]=='-':
+                                    imagen = Imagen.Imagen("otros/bolas", i+160, j+110, tamCuadro, tamCuadro)
+                                    screen.blit(imagen.imagen, (i+160, j+110))
+                                    n = Nodo.Nodo(id,self.mapaJ[j1][i1],imagen)
+                                    mapa_imagenes[j1][i1] = n
+                                id += 1
+                            else:
+                                
+                                if self.mapaJ[j1][i1]=='#':
+                                    if img != None:
+                                        screen.blit(mapa_imagenes[j1][i1].imagen.imagen, (i+155, j+105))
+                                if self.mapaJ[j1][i1]=='@':
+                                    posicion_x_pacman = j1
+                                    posicion_y_pacman = i1
+                                    imagen = Imagen.Imagen("otros/PacMan", i+160, j+110, tamCuadro+8, tamCuadro+8)
+                                    screen.blit(imagen.imagen, (i+156, j+106))
+                                if self.mapaJ[j1][i1]=='_' or self.mapaJ[j1][i1]=='-':
+                                    imagen = Imagen.Imagen("otros/bolas", i+160, j+110, tamCuadro, tamCuadro)
+                                    screen.blit(imagen.imagen, (i+160, j+110))
                             j1 += 1
                     i1 += 1
-                        
+            primera_vandera = False
             pygame.display.flip()
-            clock.tick(1)
+            #clock.tick(1)
         pygame.quit()
 
 
     def moverPacman(self,evento,posicion_x_pacman,posicion_y_pacman,bandera):
-        if evento.type == pygame.KEYUP and bandera == True:
-            bandera = False
-            if evento.key == pygame.K_RIGHT:
-                if(self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]!='#'):
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]='@'
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+        
+        if evento.key == pygame.K_RIGHT:
+            if(self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]!='#'):
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman+1]='@'
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
             
-            elif evento.key == pygame.K_LEFT:
-                if(self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]!='#'):
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]='@'
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
-            elif evento.key == pygame.K_DOWN:
-                if(self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]!='#'):
-                    self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]='@'
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
-            elif evento.key == pygame.K_UP:
-                if(self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]!='#'):
-                    self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]='@'
-                    self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+        elif evento.key == pygame.K_LEFT:
+            if(self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]!='#'):
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman-1]='@'
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+        elif evento.key == pygame.K_DOWN:
+            if(self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]!='#'):
+                self.mapaJ[posicion_x_pacman+1][posicion_y_pacman]='@'
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
+        elif evento.key == pygame.K_UP:
+            if(self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]!='#'):
+                self.mapaJ[posicion_x_pacman-1][posicion_y_pacman]='@'
+                self.mapaJ[posicion_x_pacman][posicion_y_pacman]=''
     #    if evento.type == pygame.keyup:
     #        if evento.key == pygame.k_right:
     #            velocidad_auto_x = 0
--- Pac-man/Pac-man/Nodo.py
+++ Pac-man/Pac-man/Nodo.py
@@ -2,7 +2,9 @@ class Nodo(object):
     def __init__(self):
         self.id = None
         self.tipo = None
+        self.imagen = None
 
-    def __init__(self, id, tipo):
+    def __init__(self, id, tipo, imagen):
         self.id = id
         self.tipo = tipo
+        self.imagen = imagen
